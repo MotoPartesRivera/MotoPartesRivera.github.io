d45f5ba08b264428baab1f829668c6d068adf68b
MotoPartesRivera <motopartesrivera1@gmail.com>
2026-01-31 13:57:51 -0600

    Update producto.js

--- JS/producto.js
+++ JS/producto.js
@@ -29,10 +29,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const producto = document.querySelector(".producto-titulo").textContent;
 
     const mensaje = `Hola, me interesa el producto ${producto}, de marca ${marca}, modelo ${modelo}, tipo ${tipo}. ¿Se encuentra disponible?`;
-    const numeroWhatsApp = "522211038688"; // tu número con código país
+    const numeroWhatsApp = "522211897589"; // tu número con código país
     const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
 
     window.open(url, "_blank");
   });
 });
+
 
